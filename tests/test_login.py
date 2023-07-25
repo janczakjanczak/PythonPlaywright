fc0746e34f8bb86234fa1959f89d1bda2b6215f8
DESKTOP-MOSQ67E\Dupyczet <dupczet3@gmail.com>
2023-07-25 22:19:31 +0200

    Preparing to google page basic tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,7 @@
 import re
 from playwright.sync_api import Page, expect
+from src.page_object.search_page import SearchPage
+from src.page_object.google_page import GooglePage
 
 
 def test_homepage(page: Page):
@@ -19,3 +21,13 @@ def test_homepage(page: Page):
 
     # Expects the URL to contain intro.
     expect(page).to_have_url(re.compile(".*intro"))
+
+def test_search(page: Page):
+    search_page = SearchPage(page)
+    search_page.navigate()
+    search_page.search("search query")
+        
+def test_new(page: Page):
+    google_page = GooglePage(page)
+    google_page.start_button.click()
+    google_page.search_for_phrase()
